7fdc3d76f4d3e92a265156406f8530722175cbab
Alex Thatcher <alex@alexthatcher.com>
2018-11-18 13:22:02 -0800

    Dependency versions added

--- package.js
+++ package.js
@@ -8,11 +8,11 @@ Package.describe({
 
 Package.onUse((api) => {
     api.use('ecmascript@0.1.2', ['client', 'server']);
-    api.use('oauth2', ['client', 'server']);
-    api.use('oauth', ['client', 'server']);
-    api.use('http', 'server');
-    api.use('random', 'client');
-    api.use('service-configuration', ['client', 'server']);
+    api.use('oauth2@1.2.1', ['client', 'server']);
+    api.use('oauth@1.2.6', ['client', 'server']);
+    api.use('http@1.4.2', 'server');
+    api.use('random@1.1.0', 'client');
+    api.use('service-configuration@1.0.11', ['client', 'server']);
 
     api.addFiles('hpid_client.js', 'client');
     api.addFiles('hpid_server.js', 'server');
