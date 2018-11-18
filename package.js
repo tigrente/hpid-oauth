67f5b481149359c02523d7dae6a07d281113235c
Alex Thatcher <alex@alexthatcher.com>
2018-11-18 11:02:35 -0800

    Prep for publication

--- package.js
+++ package.js
@@ -1,18 +1,21 @@
 Package.describe({
-  summary: 'HP ID OAuth flow',
-  version: '1.0.0',
+    name: 'tigrente:hpid-oauth',
+    git: 'https://github.com/tigrente/hpid-oauth',
+    documentation: 'README.md',
+    summary: 'HP ID OAuth flow',
+    version: '1.0.0',
 });
 
 Package.onUse((api) => {
-  api.use('ecmascript', ['client', 'server']);
-  api.use('oauth2', ['client', 'server']);
-  api.use('oauth', ['client', 'server']);
-  api.use('http', 'server');
-  api.use('random', 'client');
-  api.use('service-configuration', ['client', 'server']);
+    api.use('ecmascript', ['client', 'server']);
+    api.use('oauth2', ['client', 'server']);
+    api.use('oauth', ['client', 'server']);
+    api.use('http', 'server');
+    api.use('random', 'client');
+    api.use('service-configuration', ['client', 'server']);
 
-  api.addFiles('hpid_client.js', 'client');
-  api.addFiles('hpid_server.js', 'server');
+    api.addFiles('hpid_client.js', 'client');
+    api.addFiles('hpid_server.js', 'server');
 
-  api.export('Hpid');
+    api.export('Hpid');
 });
